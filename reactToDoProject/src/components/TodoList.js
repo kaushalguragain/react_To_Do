3aabf21e8743eb0fc23b28df85d264295dfb72a1
kaushalguragain <kaushalguragain123@gmail.com>
2019-03-13 10:14:52 +0545

    making list empty

--- reactToDoProject/src/components/TodoList.js
+++ reactToDoProject/src/components/TodoList.js
@@ -3,9 +3,9 @@ import React from 'react'
 const TodoList =({todos,deleteTodo}) =>{ //descructuring needs curly barces
   return(
     <div className="collection">
-        {todos.map(todo => <p key={todo.id} onClick={(e) =>deleteTodo(todo.id)} className="collection-item">{todo.content}
+        {todos.length >0 ? todos.map(todo => <p key={todo.id} onClick={(e) =>deleteTodo(todo.id)} className="collection-item">{todo.content}
           
-          </p>)}
+          </p>):<p>List is empty Add new Items</p> }
     </div>
   )
 }
